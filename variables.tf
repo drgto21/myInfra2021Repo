9c56cbddb6d61e0d3b6f6f64230c2ea8cfae8f75
drgto21 <110442291+drgto21@users.noreply.github.com>
2023-11-21 19:38:13 -0500

    Update variables.tf
    change to ssh key

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "myJenkinsKey23NVA" 
 }
 
 variable "instance_type" { 
